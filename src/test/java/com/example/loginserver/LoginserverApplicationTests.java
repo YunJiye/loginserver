7107a8949b5cef1f41c23fbe9fcc9644f17db96f
yunjiye <yjo3440@naver.com>
2024-07-01 07:49:16 +0900

    JPA Auditing Test code

--- src/test/java/com/example/loginserver/LoginserverApplicationTests.java
+++ src/test/java/com/example/loginserver/LoginserverApplicationTests.java
@@ -2,24 +2,30 @@ package com.example.loginserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.context.annotation.Import;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.example.loginserver.configuration.JpaAuditingConfiguration;
+import com.example.loginserver.domain.userinfo.UserInfo;
+import com.example.loginserver.domain.userinfo.UserInfoRepository;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Import(JpaAuditingConfiguration.class)
 class LoginserverApplicationTests {
-
-	/*@Test
-	void contextLoads() {
-	}*/
 
 	@Autowired
     private MockMvc mockMvc;
@@ -35,4 +41,34 @@ class LoginserverApplicationTests {
                 .andReturn();
 	}
 
+    @Autowired
+    private UserInfoRepository userInfoRepository;
+
+    @Test
+    public void testJpaAuditing() throws Exception{
+        // given
+        UserInfo userInfo = new UserInfo("testUser", "password");
+
+        // when
+        userInfo = userInfoRepository.save(userInfo);   // save user
+
+        // then
+        assertNotNull(userInfo.getUniqueid()); // id should be generated
+        assertNotNull(userInfo.getCreatedDate()); // createdAt should be set
+        assertNotNull(userInfo.getModifiedDate()); // updatedAt should be set
+
+        // Remember the initial modifiedDate
+        LocalDateTime initialModifiedDate = userInfo.getModifiedDate();
+
+        // update user info
+        LocalDateTime createdAt = userInfo.getCreatedDate();
+        userInfo.setUserpwd("newPassword");
+        userInfo = userInfoRepository.save(userInfo);
+
+        // createdAt should not change
+        assertNotNull(userInfo.getCreatedDate());
+        assertNotNull(userInfo.getModifiedDate());
+        assert createdAt.equals(userInfo.getCreatedDate()); // createdAt should not change
+        assertNotEquals(initialModifiedDate, userInfo.getModifiedDate());
+    }
 }
